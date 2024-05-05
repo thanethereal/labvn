e41c113e48d518c6c4f07096bee8848cae1a21d0
unknown <thanvm2000@gmailcom>
2024-05-05 15:52:30 +0700

    push code

--- web/app.py
+++ web/app.py
@@ -18,33 +18,6 @@ import route_header_image
 import route_footer
 import os
 from flask import request
-# UPLOAD_FOLDER = 'D:/labvn/web/static/uploads/'
-# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# @app.route('/uploads')
-# def index():
-#     return render_template('uploads.html')
-
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if 'file' not in request.files:
-#         return 'No file part'
-#     file = request.files['file']
-#     if file.filename == '':
-#         return 'No selected file'
-#     if file and allowed_file(file.filename):
-#         filename = file.filename
-#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#         return 'File uploaded successfully'
-#     else:
-#         return 'File type not allowed'
-
-
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
 
